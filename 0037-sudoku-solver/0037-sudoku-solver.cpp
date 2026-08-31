a28db2746e9fc49c22fbbc74f8cc3e3e1a3e58ef
ishmeet01-dotcom <ishmeet1180@gmail.com>
2026-08-31 21:37:22 +0530

    Time: 372 ms (44.23%), Space: 8.9 MB (20.35%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        backtrack(board);
+    }
+
+private:
+    bool backtrack(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (backtrack(board)) return true;
+                            board[row][col] = '.'; 
+                        }
+                    }
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) return false;
+            int subRow = 3 * (row / 3) + i / 3;
+            int subCol = 3 * (col / 3) + i % 3;
+            if (board[subRow][subCol] == num) return false;
+        }
+        return true;
+    }
+};
+
